9b9fc2685c9c2329ce52a45c3c1994ccf1e6c6aa
Mamang touthang <huebart6@gmail.com>
2022-03-24 20:13:38 +0530

    Initial commit

---
+++ src/test/java/mang/io/eurekaserverapplication/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mang.io.eurekaserverapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
